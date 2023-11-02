4053ef226798a125a71220c388ba9074e15ca0f8
Hadi <hadibakhshi277@gmail.com>
2023-11-02 18:04:47 +0330

    feat: add some condition check
    add if statement, ternary and switch case

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,9 +16,49 @@ namespace ConsoleApp1
 
         // ----------------- END OF FUNCTIONS -----------------
 
-        static void Main(string[] args)
+        static void Main()
         {
+            // Relational Operators : > < >= <= == != 
+            // Logical Operators : && || !
+            Console.WriteLine("Plase enter your name");
 
-        }    
+            if (int.TryParse(Console.ReadLine(), out int age))
+            {
+                if ((age >= 5) && (age <= 7))
+                {
+                    Console.WriteLine("Go to elementary school");
+                }
+                if ((age > 7) && (age < 13))
+                {
+                    Console.WriteLine("Go to middle school");
+                }
+                if ((age > 13) && (age <= 19))
+                {
+                    Console.WriteLine("Go to high school");
+                }
+                else
+                {
+                    Console.WriteLine("Go to college");
+                }
+            }
+
+            bool canDrive = age >= 16 ? true : false;
+
+            switch(age)
+            {
+                case 1:
+                case 2:
+                    Console.WriteLine("Go to hell");
+                    break;
+                case 3:
+                case 4:
+                    Console.WriteLine("Go to heaven");
+                    break;
+                default:
+                    Console.WriteLine("What the hell");
+                    // goto OtherSchool; This is not a good practice
+                    break;
+            }
+        }
     }
 }
