934d8e35429f87ff3c9d6562efb0bd0760d5ff1b
Hadi <hadibakhshi277@gmail.com>
2023-11-02 18:46:29 +0330

    feat: add while and do while loops

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,6 +16,36 @@ namespace ConsoleApp1
 
         // ----------------- END OF FUNCTIONS -----------------
 
-        static void Main(string[] args) {}    
+        static void Main() 
+        {
+            int i = 1;
+            while (i <= 10)
+            {
+                if(i % 2 == 0)
+                {
+                    i++;
+                    continue;
+                }
+                if (i == 9) break;
+                Console.WriteLine(i);
+                i++;
+            }
+
+
+            // Do while
+            Random rnd = new Random();
+            int secretNumber = rnd.Next(1,11);
+            int numberGuessed = 0;
+            Console.WriteLine("Random Num: ", secretNumber);
+
+            do
+            {
+                Console.WriteLine("Enter a number between 1 and 10");
+                numberGuessed = Convert.ToInt32(Console.ReadLine());
+
+            } while (secretNumber != numberGuessed);
+            
+            Console.WriteLine("You guessed it, it was {0}",numberGuessed);
+        }
     }
 }
