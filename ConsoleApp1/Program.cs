0fe24f0c83f2cc4afc66dea9a9a014f990ca2fed
Hadi <hadibakhshi277@gmail.com>
2023-11-02 23:00:45 +0330

    feat: add exception handling logic

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,8 +14,42 @@ namespace ConsoleApp1
             }
         }
 
+        static double DoDivision(double x, double y)
+        {
+            if(y == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return x / y;
+        }
+
         // ----------------- END OF FUNCTIONS -----------------
 
-        static void Main(string[] args) {}    
+        static void Main(string[] args) {
+            double num1 = 5;
+            double num2 = 0;
+
+            try
+            {
+                Console.WriteLine("5 / 0 = {0}", DoDivision(num1, num2));
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("You can not divide by zero", ex.Message);
+                Console.WriteLine(ex.GetType().Name);
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.GetType().Name);
+            }
+            finally
+            {
+                Console.WriteLine("cleaning up");
+            }
+        
+        }    
     }
 }
