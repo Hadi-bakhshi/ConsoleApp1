ebe28b7e1ab5aff87363cd9cde80d6510bdbc1aa
Hadi <hadibakhshi277@gmail.com>
2023-11-03 12:25:31 +0330

    feat: manipulate strings using string builder

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System; // it is going to import the namespace that has some classes and functions
+using System.Globalization;
+using System.Text;
 
 namespace ConsoleApp1
 {
@@ -16,6 +18,30 @@ namespace ConsoleApp1
 
         // ----------------- END OF FUNCTIONS -----------------
 
-        static void Main(string[] args) {}    
+        static void Main(string[] args) 
+        { 
+            StringBuilder sb = new StringBuilder("Random text");
+            StringBuilder sb2 = new StringBuilder("More stuff that is very important",256);
+
+            Console.WriteLine("Capacity : {0}", sb2.Capacity);
+            Console.WriteLine("Length : {0}", sb.Length);
+            sb2.AppendLine(" More important text");
+            Console.WriteLine(sb2.ToString());
+
+            CultureInfo enUs = CultureInfo.CreateSpecificCulture("en-US");
+            string bestCust = "Hadi Bakhshi";
+            sb2.AppendFormat(enUs, "Best Customer : {0}", bestCust);
+            Console.WriteLine(sb2.ToString());
+            sb2.Replace("text", "characters");
+            Console.WriteLine(sb2.ToString());
+            sb2.Clear();
+            sb2.Append("Random Text");
+            Console.WriteLine(sb.Equals(sb2));
+            sb2.Insert(11, " that is great");
+            Console.WriteLine(sb2.ToString());
+            sb2.Remove(11, 7);
+            Console.WriteLine(sb2.ToString());
+
+        }    
     }
 }
