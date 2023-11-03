5d7ea7992954dd2d0d016893ca8af1b17a59fcba
Hadi <hadibakhshi277@gmail.com>
2023-11-03 12:41:03 +0330

    feat: merge all sections to the main

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System; // it is going to import the namespace that has some classes and functions
+using System.Globalization;
+using System.Text;
 
 namespace ConsoleApp1
 {
@@ -12,7 +14,6 @@ namespace ConsoleApp1
                 Console.WriteLine("{0}: {1}", msg, i);
             }
         }
-
         static void ArraysSection()
         {
 
@@ -97,12 +98,148 @@ namespace ConsoleApp1
             Console.WriteLine("Trim : {0}", randString.Trim());
             Console.WriteLine(@"Exactly what I type\n");
         }
+        static double DoDivision(double x, double y)
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return x / y;
+        }
+        static void ExceptionHandlingSection()
+        {
+            double num1 = 5;
+            double num2 = 0;
+            try
+            {
+                Console.WriteLine("5 / 0 = {0}", DoDivision(num1, num2));
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("You can not divide by zero", ex.Message);
+                Console.WriteLine(ex.GetType().Name);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.GetType().Name);
+            }
+            finally
+            {
+                Console.WriteLine("cleaning up");
+            }
+        }
+        static void LoopsSection()
+        {
+            int i = 1;
+            while (i <= 10)
+            {
+                if (i % 2 == 0)
+                {
+                    i++;
+                    continue;
+                }
+                if (i == 9) break;
+                Console.WriteLine(i);
+                i++;
+            }
+
+
+            // Do while
+            Random rnd = new Random();
+            int secretNumber = rnd.Next(1, 11);
+            int numberGuessed = 0;
+            Console.WriteLine("Random Num: ", secretNumber);
+
+            do
+            {
+                Console.WriteLine("Enter a number between 1 and 10");
+                numberGuessed = Convert.ToInt32(Console.ReadLine());
+
+            } while (secretNumber != numberGuessed);
+
+            Console.WriteLine("You guessed it, it was {0}", numberGuessed);
+        }
+        static void OperatorsSection()
+        {
+            // Relational Operators : > < >= <= == != 
+            // Logical Operators : && || !
+            Console.WriteLine("Plase enter your name");
+
+            if (int.TryParse(Console.ReadLine(), out int age))
+            {
+                if ((age >= 5) && (age <= 7))
+                {
+                    Console.WriteLine("Go to elementary school");
+                }
+                if ((age > 7) && (age < 13))
+                {
+                    Console.WriteLine("Go to middle school");
+                }
+                if ((age > 13) && (age <= 19))
+                {
+                    Console.WriteLine("Go to high school");
+                }
+                else
+                {
+                    Console.WriteLine("Go to college");
+                }
+            }
+
+            bool canDrive = age >= 16 ? true : false;
+
+            switch (age)
+            {
+                case 1:
+                case 2:
+                    Console.WriteLine("Go to hell");
+                    break;
+                case 3:
+                case 4:
+                    Console.WriteLine("Go to heaven");
+                    break;
+                default:
+                    Console.WriteLine("What the hell");
+                    // goto OtherSchool; This is not a good practice
+                    break;
+            }
+        }
+        static void StringBuilderSection()
+        {
+            StringBuilder sb = new StringBuilder("Random text");
+            StringBuilder sb2 = new StringBuilder("More stuff that is very important", 256);
+
+            Console.WriteLine("Capacity : {0}", sb2.Capacity);
+            Console.WriteLine("Length : {0}", sb.Length);
+            sb2.AppendLine(" More important text");
+            Console.WriteLine(sb2.ToString());
+
+            CultureInfo enUs = CultureInfo.CreateSpecificCulture("en-US");
+            string bestCust = "Hadi Bakhshi";
+            sb2.AppendFormat(enUs, "Best Customer : {0}", bestCust);
+            Console.WriteLine(sb2.ToString());
+            sb2.Replace("text", "characters");
+            Console.WriteLine(sb2.ToString());
+            sb2.Clear();
+            sb2.Append("Random Text");
+            Console.WriteLine(sb.Equals(sb2));
+            sb2.Insert(11, " that is great");
+            Console.WriteLine(sb2.ToString());
+            sb2.Remove(11, 7);
+            Console.WriteLine(sb2.ToString());
+        }
         // ----------------- END OF FUNCTIONS -----------------
 
         static void Main(string[] args) 
         {
             StringSection();
             ArraysSection();
+            ExceptionHandlingSection();
+            LoopsSection();
+            OperatorsSection();
+            StringBuilderSection();
         }    
     }
 }
